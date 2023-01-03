b018d0d53e5a4206f8896996b6763d746b884723
barathviknesh <barathviknesh.9566@gmail.com>
2023-01-03 11:34:03 +0530

    version upgrated

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -74,7 +74,7 @@ const razorpayCall = ({
             }),
             cache: 'default'
           }).then(res => res.json());
-          alert(result.data.message);
+          alert(result.message);
         },
         prefill: {
           name: preFillName,
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -70,7 +70,7 @@ const razorpayCall = ({
             }),
             cache: 'default'
           }).then(res => res.json());
-          alert(result.data.message);
+          alert(result.message);
         },
         prefill: {
           name: preFillName,
